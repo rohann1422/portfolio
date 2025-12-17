675ca1c32ef2c6bb9a183af2bf1f4cf937c0986b
rohann1422 <rohansidd9@gmail.com>
2025-12-17 17:28:07 +0530

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { motion } from 'framer-motion'
 import { FaGithub, FaLinkedin, FaEnvelope, FaArrowUp } from 'react-icons/fa'
 import './Footer.css'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { FaBars, FaTimes } from 'react-icons/fa'
